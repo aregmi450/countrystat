1e8b9d8ddd596dc1ef8fc198322109fb36b6688e
Arun Regmi <aregmi450@gmail.com>
2022-02-03 11:45:00 +0545

    added navigation to app

--- App.tsx
+++ App.tsx
@@ -1,4 +1,7 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import { StatusBar } from "expo-status-bar";
+import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import AddCountry from "./components/AddCountry";
 import ChooseCountry from "./components/CountryName";
@@ -6,15 +9,35 @@ import Details from "./screens/DetailsScreen";
 import Hello from "./screens/HomeScreen";
 import HomeScreen from "./screens/HomeScreen";
 
-export default function App() {
+// move the stack
+const Stack = createStackNavigator();
+
+function MainStackNavigator() {
   return (
-    <View style={styles.container}>
-      <Hello />
-      <Details />
-      <AddCountry />
-    </View>
+    <Stack.Navigator initialRouteName="HomeScreen">
+      <Stack.Screen name={"Home"} component={HomeScreen} />
+      <Stack.Screen name={"Details"} component={Details} />
+    </Stack.Navigator>
   );
 }
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MainStackNavigator />
+    </NavigationContainer>
+  );
+}
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Hello />
+//       <Details />
+//       <AddCountry />
+//     </View>
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -3,13 +3,14 @@ import { useTailwind } from "tailwind-rn/dist";
 import { Text, View, StyleSheet } from "react-native";
 import CountryFlag from "react-native-country-flag";
 import SearchBar from "./SearchBar";
+import ChooseCountry from "../components/CountryName";
+import AddCountry from "../components/AddCountry";
 
 const HomeScreen = () => {
   // const tailwind = useTailwind();
   return (
     <View style={styles.header}>
       <Text style={styles.text}> GO ABROAD </Text>
-
       <Text>Choose the country of your dreams here ...</Text>
     </View>
   );
