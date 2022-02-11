b363be98a4bc2c63e81cec220ab33991a8c01f96
Arun Regmi <aregmi450@gmail.com>
2022-02-11 12:29:08 +0545

    deleting the unwanted codes

--- src/components/Collapsible.tsx
+++ src/components/Collapsible.tsx
@@ -1,30 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import { View } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
-import Collapsible from "react-native-collapsible";
 import Details from "./BasicRequirements";
 import LinkSection from "./LinkSection";
-import Constant from "./Constants";
 import Language from "./Language";
 import LanguageLink from "./LanguageLinks";
 import CollapseWindow from "./CollapsibleData";
 
 const CollapseSection = () => {
-  const [collapsed, setCollapsed] = useState(true);
-  const toggleExpanded = () => {
-    // Toggling the state of single Collapsible
-    setCollapsed(!collapsed);
-  };
-
   return (
     <View>
-      {/* <TouchableOpacity onPress={toggleExpanded}>
-        <Constant headline="Entry Requirements" />
-        <Collapsible collapsed={collapsed} align="top">
-          <Details />
-        </Collapsible>
-      </TouchableOpacity> */}
-
       <CollapseWindow
         headline="Entry Requirements"
         componentName={<Details />}
@@ -43,27 +27,6 @@ const CollapseSection = () => {
         headline="Places to Study Native Language"
         componentName={<LanguageLink />}
       />
-
-      {/* <TouchableOpacity onPress={toggleExpanded}>
-        <Constant headline="Useful Links for Future Students " />
-        <Collapsible collapsed={collapsed} align="top">
-          <LinkSection />
-        </Collapsible>
-      </TouchableOpacity>
-
-      <TouchableOpacity onPress={toggleExpanded}>
-        <Constant headline="Language Requirements" />
-        <Collapsible collapsed={collapsed} align="top">
-          <Language />
-        </Collapsible>
-      </TouchableOpacity>
-
-      <TouchableOpacity onPress={toggleExpanded}>
-        <Constant headline="Places to Study Native Language" />
-        <Collapsible collapsed={collapsed} align="top">
-          <LanguageLink />
-        </Collapsible>
-      </TouchableOpacity> */}
     </View>
   );
 };
