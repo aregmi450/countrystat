a68292023a055ec6ca9ad060e2878ce82ec4e878
Arun Regmi <aregmi450@gmail.com>
2022-02-11 13:18:46 +0545

    adding tailwind css for better styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  content: ["./**/*.{js,jsx,ts,tsx}"],
+  content: [],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+  corePlugins: require('tailwind-rn/unsupported-core-plugins'),
+}
